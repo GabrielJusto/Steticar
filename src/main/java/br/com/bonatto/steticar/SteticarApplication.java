ecbed1a23344738ea4b089e61d37f38a0e05e6cb
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2024-01-24 10:46:25 -0300

    projeto iniciado

---
+++ src/main/java/br/com/bonatto/steticar/SteticarApplication.java
@@ -0,0 +1,13 @@
+package br.com.bonatto.steticar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteticarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteticarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bonatto/steticar/SteticarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bonatto.steticar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteticarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
