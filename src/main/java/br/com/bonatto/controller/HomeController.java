074b4d5b7cfedba5ae5a3f0316da099ad20227a2
Gabriel Bonatto <gabriel.justo@acad.pucrs.br>
2024-01-24 11:19:16 -0300

    docker configurado

---
+++ src/main/java/br/com/bonatto/controller/HomeController.java
@@ -0,0 +1,19 @@
+package br.com.bonatto.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    
+
+    @GetMapping
+    public ResponseEntity abreHome()
+    {
+        return ResponseEntity.ok().build();
+    }
+}
